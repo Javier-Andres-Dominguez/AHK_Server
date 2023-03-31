5632e16fe730455d920362783d43a23508bc52d8
Javier Andrés Domínguez <72435329+Javier-Andres-Dominguez@users.noreply.github.com>
2023-03-31 16:19:28 +0200

    Add files via upload

---
+++ AHK_Server/src/main/java/models/User_Subscribe_UserId.java
@@ -0,0 +1,61 @@
+package models;
+
+import java.io.Serializable;
+import java.util.Objects;
+
+import javax.persistence.Embeddable;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+
+@Embeddable
+public class User_Subscribe_UserId implements Serializable{
+
+	@ManyToOne
+	@JoinColumn(name="SubscribedUserId")
+	private User userSubscribed;
+	@ManyToOne
+	@JoinColumn(name="SubscribedToUserId")
+	private User subscribedToUser;
+	
+	public User_Subscribe_UserId() {
+
+	}
+
+	public User_Subscribe_UserId(User userSubscribed, User subscribedToUser) {
+		this.userSubscribed = userSubscribed;
+		this.subscribedToUser = subscribedToUser;
+	}
+
+	public User getUserSubscribed() {
+		return userSubscribed;
+	}
+
+	public void setUserSubscribed(User userSubscribed) {
+		this.userSubscribed = userSubscribed;
+	}
+
+	public User getSubscribedToUser() {
+		return subscribedToUser;
+	}
+
+	public void setSubscribedToUser(User subscribedToUser) {
+		this.subscribedToUser = subscribedToUser;
+	}
+
+	public boolean equals(Object other) {
+		if ((this == other))
+			return true;
+		if ((other == null))
+			return false;
+		if (!(other instanceof User_Subscribe_UserId))
+			return false;
+		User_Subscribe_UserId castOther = (User_Subscribe_UserId) other;
+
+		return Objects.equals(userSubscribed, castOther.userSubscribed) && Objects.equals(subscribedToUser, castOther.subscribedToUser);
+
+	}
+
+	public int hashCode() {
+		return Objects.hash(userSubscribed, subscribedToUser);
+	}
+}
