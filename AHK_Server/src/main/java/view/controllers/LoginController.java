465116e3bdddcd91e6e5ec868e26a90cf2747b83
Javier Andrés Domínguez <72435329+Javier-Andres-Dominguez@users.noreply.github.com>
2023-04-19 23:42:57 +0200

    Add files via upload

--- AHK_Server/src/main/java/view/controllers/LoginController.java
+++ AHK_Server/src/main/java/view/controllers/LoginController.java
@@ -1,9 +1,7 @@
 package view.controllers;
 
-import java.io.BufferedReader;
 import java.io.BufferedWriter;
 import java.io.File;
-import java.io.FileReader;
 import java.io.FileWriter;
 import java.util.List;
 
